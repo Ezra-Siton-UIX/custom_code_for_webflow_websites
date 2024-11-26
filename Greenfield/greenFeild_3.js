eb2308d83faf4acfdb4e2fe64c2aa34c16346f93
Ezra siton <siton22@hotmail.com>
2024-11-26 22:33:23 +0200

    Update and rename greenFeild_2.js to greenFeild_3.js

--- Greenfield/greenFeild_3.js
+++ Greenfield/greenFeild_3.js
@@ -19,8 +19,8 @@
 /* added under webflow as CDN */
 /* added under webflow as CDN */
 
+/**>>>>>>>>>> 1 of 13 <<<<<<<<<<**/
 /* ### change url to url category param of all footer category links */
-
 
 $("[category_footer_link]" ).each(function( index ) {
   let this_url = $( this ).attr("href");
@@ -29,13 +29,11 @@ $("[category_footer_link]" ).each(function( index ) {
 });
 
 /* ### stop bubble linkedin ### */
-
-
 $("[linkedin_btn]").click(function(event){
   event.stopPropagation();
 });
 
-
+/**>>>>>>>>>> 2 of 13 <<<<<<<<<<**/
 /*### change url of company modal #### */
 
 $("[company_url]" ).each(function( index ) {
@@ -46,6 +44,7 @@ $("[careers_url]" ).each(function( index ) {
   $( this ).attr("target", "_blank");
 });
 
+/**>>>>>>>>>> 3 of 13 <<<<<<<<<<**/
 /*### set the url on hidden form feild (To know the page the user send the form from) ###*/
 
 const page_url = window.location.pathname;
@@ -55,19 +54,19 @@ $("input[page_url]" ).each(function( index ) {
   $( this ).attr("type", "hidden");
 });
 
-
+/**>>>>>>>>>> 4 of 10 <<<<<<<<<<**/
+/*### clean up the CMS url text (From https://hello.com to "hello.com") ##*/
 $("[company_website_link]" ).each(function( index ) {
   let this_full_url = $( this ).text();
   this_full_url = this_full_url.replace(/^https?:\/\//, '');
   if(this_full_url.at(-1) == "/"){
     this_full_url = this_full_url.slice(0, -1);
-    console.log(this_full_url);
   }
   $( this ).text(this_full_url);
 });
 
-
-/*#### 0 of 6 CTA modal radio button - add active class on click ####*/
+/**>>>>>>>>>> 5 of 13 <<<<<<<<<<**/
+/*#### Modal Form CTA modal radio button - add active class on click ####*/
 $("[cta-radio_btn]").removeClass("active")
 
 $("[cta-radio_btn]").click(function(){
@@ -75,7 +74,8 @@ $("[cta-radio_btn]").click(function(){
   $(this).addClass("active")
 });
 
-/*#### 0 of 6 CTA modal fancybox ####*/
+/**>>>>>>>>>> 6 of 13 <<<<<<<<<<**/
+/*#### CTA modal fancybox ####*/
 Fancybox.bind("[cta][data-fancybox]", {
   // Your custom options
   closeButton: false,
@@ -101,7 +101,9 @@ Fancybox.bind("[cta][data-fancybox]", {
   },
 });
 
-/*#### 0 of 6 ####*/
+
+/**>>>>>>>>>> 7 of 13 <<<<<<<<<<**/
+/*### Fancybox Companies Modal ###*/
 
 if($("[data-src][data-fancybox]").length > 0){
 
@@ -127,7 +129,6 @@ if($("[data-src][data-fancybox]").length > 0){
     on: {
       reveal: (fancybox, slide) => {
         // The content of this slide is loaded and ready to be revealed
-        console.log("reveal companies");
         //lenis.stop()
         const company_popup_image_overlay = slide.el.querySelector("[company_popup_image_overlay]");
 
@@ -135,19 +136,19 @@ if($("[data-src][data-fancybox]").length > 0){
           company_popup_image_overlay.style.opacity = "0.4";
         }, 200);
 
-
         lenis.destroy();
-
-
       },
       close: (fancybox, slide) => {
         // The content of this slide is loaded and ready to be revealed
-        console.log("close companies");
         //lenis.start()
         lenis.start()
       },
     },
   });
+
+
+  /**>>>>>>>>>> 8 of 13 <<<<<<<<<<**/
+  /*### Fancybox Team Modal ###*/
 
   Fancybox.bind("[team] [data-fancybox]", {
     // Your custom options
@@ -176,52 +177,39 @@ if($("[data-src][data-fancybox]").length > 0){
 
 
 
-/*#### 0 of 5 ####*/
-if(window.location.pathname == "/"){
-  $( "[company_card]" ).each(function( index ) {
-    var inview = new Waypoint.Inview({
-      element: $("[company_card]")[index],
-      enter: function(direction) {
+/*#### add active class to card on scroll - 0 of 5
+      if(window.location.pathname == "/"){
+        $( "[company_card]" ).each(function( index ) {
+          var inview = new Waypoint.Inview({
+            element: $("[company_card]")[index],
+            enter: function(direction) {
 
 
-      },
-      entered: function(direction) {
-        $("[company_card]").removeClass("is_active");
-        $(this.element).addClass("is_active");
-      },
-      exit: function(direction) {
+            },
+            entered: function(direction) {
+              $("[company_card]").removeClass("is_active");
+              $(this.element).addClass("is_active");
+            },
+            exit: function(direction) {
 
-      },
-      exited: function(direction) {
-        $(this.element).removeClass("is_active");
+            },
+            exited: function(direction) {
+              $(this.element).removeClass("is_active");
 
-      },
-      offset: '60%'
-    })
+            },
+            offset: '60%'
+          })
 
-    });
-}
+          });
+      }
+       ####*/
 
+/**>>>>>>>>>> 9 of 10 <<<<<<<<<<**/
+/*### Navbar Animation on srcoll ###*/
 
-
-/*#### 0 of 5 ####*/
-if(document.querySelector("[navbar]") !== null){
-  const navbar_height = document.querySelector("[navbar]").clientHeight;
-  if(document.querySelector("[hero_section]") !== null){
-    // document.querySelector("[hero_section]").style.setProperty('min-height', `calc(100svh - ${navbar_height}px)`);
-  }
-}
-
-
-
-/*#### 0 of 4 navbar sticky trick ####*/
-/* NAVBAR ANIMATION */
 /* check if navbar exists (to avoid js console errors) */
 let exists = $( "[navbar]" ).length ? true : false;
-/* if user scroll add active class */
-$(window).on("scroll", function() {
 
-});
 
 $(function(){
   var lastScrollTop = 0, delta = 5;
@@ -244,7 +232,6 @@ $(function(){
           // SCROLLING DOWN 
           $("[navbar]").css("transform", "translate3d(0px, -110%, 0px");
 
-
         } else {
           // ACTION ON
           // SCROLLING UP 
@@ -258,11 +245,11 @@ $(function(){
 });
 
 
-/*#### 1 of 3 - swiper_hero_logos #######*/
-/* show modals on hover */
-//$("[diagram_modal_overlay]").hide();
-$( "[diagram_modal_overlay]" ).css("transform", `translateY(40px)`);
+/**>>>>>>>>>> 10 of 13 <<<<<<<<<<**/
+/*### Diagram Desktop hover Setting // Mobile Swiper Diagram  ###*/
 
+/* Part 10.1 - show modals on hover */
+$( "[diagram_modal_overlay]" ).css("transform", `translateY(40px)`);
 
 $( "[diagram_col]" ).hover(
   function() {/* hover in */
@@ -270,8 +257,6 @@ $( "[diagram_col]" ).hover(
 
     $( "[diagram_modal_overlay]" ).eq(this_col_index).css("transform", `translateY(0px)`);
     $( "[diagram_modal_overlay]" ).eq(this_col_index).css("opacity", 1);
-
-
 
   }, function() {/* hover out */
     const this_col_index = $( this ).attr("diagram_col");
@@ -282,18 +267,14 @@ $( "[diagram_col]" ).hover(
   }
 );
 
-
 const is_swiper_diagram_stages_slider = $(".swiper[diagram_stages_slider]").length > 0;
+
+/* Part 10.2 - Diagram Mobile Slider */
 
 const diagram_content_slider_options = {
   // Optional parameters
-
-
   centeredSlides: true,
   slideToClickedSlide: true,
-
-
-
   loop: false,
   spaceBetween: 32,
   centeredSlides: true,
@@ -319,11 +300,10 @@ const diagram_content_slider_options = {
   },
 };
 
+/* Part 10.3 - Diagram Mobile Navbar Slider */
 
 const diagram_navbar_slider_options = {
   // Optional parameters
-
-
   loop: false,
   spaceBetween: 42,
   centeredSlides: true,
@@ -360,7 +340,9 @@ if(is_swiper_diagram_stages_slider){
 
 /* ######################################## */
 
-/*#### 2 of 3 - swiper_hero_logos #######*/
+/**>>>>>>>>>> 11 of 13 <<<<<<<<<<**/
+/*### swiper_hero_logos  ###*/
+
 const is_swiper_hero_logos = $(".swiper[hero_logos]").length > 0;
 
 if(is_swiper_hero_logos){
@@ -377,6 +359,13 @@ if(is_swiper_hero_logos){
       delay: 4000,
       disableOnInteraction: false,
       pauseOnMouseEnter: false
+    },
+
+    a11y: {
+      slideRole: 'listitem',
+      containerMessage: "Greenfield Companies",
+      containerRole: "list 2",
+      itemRoleDescriptionMessage: "Company Item"
     },
 
     breakpoints: {
@@ -407,16 +396,65 @@ if(is_swiper_hero_logos){
 
 
   setTimeout(function() {
-
-
     swiper_hero_logos.slideTo(slide_to_where, 400);
   }, 500);
 
 }/* end if */
 
-/* ######################################## */
 
-/*#### 2 of 3 - marquee_cms_item #######*/
+
+/**>>>>>>>>>> 12 of 13 <<<<<<<<<<**/
+/** Random the companies cards fade in / out under Homepage **/
+
+if(window.location.pathname == "/"){
+
+
+  const cards = $("[shuffle_grid] [card]");
+  const slice_by = 4; /* how many item to show */
+  const fade_in = 1000;
+  const fade_out = 1000;
+  const delay = 2000;
+
+  $(`[shuffle_grid] [card]:nth-child(n+${slice_by + 1 })`).css("display", "block")
+  $(`[shuffle_grid] [card]:nth-child(n+${slice_by + 1 })`).css("opacity", "1")
+
+  cards.fadeOut(0);
+
+  let all_companies_array = [];
+  cards.each(function( index ) {
+    all_companies_array.push(index.toString());
+  });
+
+  const scrambled_on_load = all_companies_array.sort(() => Math.random() - 0.5);
+  var items_to_remove = scrambled_on_load.slice(0, slice_by);
+  getRandomFoods(4, items_to_remove);
+
+  function getRandomFoods(count, items_to_remove) {
+    // Scrambling the array
+    const scrambled = all_companies_array.sort(() => Math.random() - 0.5);
+
+    var newList = scrambled.filter(function(word){
+      return !items_to_remove.includes(word);
+    })
+
+    const newList_slice = newList.slice(0, slice_by);
+
+    for (let i = 0; i < slice_by; i++) { 
+      cards.eq(newList_slice[i]).fadeIn(fade_in).delay(delay).fadeOut(fade_out).promise().done(
+        function(){
+          if(i == slice_by - 1){
+            getRandomFoods(slice_by, items_to_remove);
+          }
+        })}/* end for loop */
+
+    items_to_remove = newList_slice;
+  }
+
+}/* end if homepage */
+
+
+/**>>>>>>>>>> 13 of 13 <<<<<<<<<<**/
+/*#### Marquee_cms_item #######*/
 
 const marquee_cms_items = $("[marquee_cms_item]");
 const insert_items_here_marquee__group = $("[marquee__group]");
@@ -424,12 +462,12 @@ const feature_bagde_wrapper = $("[feature_bagde_wrapper]"); /* item under webflo
 const marquee__group_parent = $("[marquee__group_parent]");
 
 /* 
-              marquee__group_parent
-                marquee__group
-                 feature_bagde_wrapper
-                   feature_badge
-                    feature_badge_logo
-              */      
+                    marquee__group_parent
+                      marquee__group
+                       feature_bagde_wrapper
+                         feature_badge
+                          feature_badge_logo
+                    */      
 
 marquee_cms_items.each(function( index ) {
   const this_marquee_cms_item_img_src = $( this ).attr("company_image_src");
@@ -447,9 +485,6 @@ $("[opacity_0_on_load]").css("opacity", 1)
 const clone = $("[marquee__group]").clone();
 marquee__group_parent.append(clone);
 
-
-
 $(".marquee__group").css("animation", "scroll-x 100s linear infinite");
 $(".marquee__group").css("margin-left", "16px");
-
 
