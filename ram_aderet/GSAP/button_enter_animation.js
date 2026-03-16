1774a4b313bc1f1f7f6c92397501fac973ce196e
Ezra siton <siton22@hotmail.com>
2026-03-16 21:26:06 +0200

    Update button_enter_animation.js

--- ram_aderet/GSAP/button_enter_animation.js
+++ ram_aderet/GSAP/button_enter_animation.js
@@ -4,6 +4,7 @@ import  { gsap_setting } from './gsap_setting.js';
 
 export function button_enter_animation(){
 
+
   ScrollTrigger.batch("[uk_button]", {
     onEnter: (batch) => {
       batch.forEach((section) => {
@@ -13,26 +14,18 @@ export function button_enter_animation(){
           duration: 0.7,
           ease: gsap_setting.easing_type,
           opacity: 1,
-          clipPath: "inset(0% 0% round 20px)",
+          clipPath: "inset(0% 0% round 20px)", // שינוי clip-path
           delay: 0,
-					onComplete: () => {
-            gsap.set(section, { 
-              clipPath: "initial",  
-              opacity: 1,
+          onComplete: () => {
+            // ברגע שסיימה את האנימציה, מחזירים את ה-clip-path ל-initial
+            gsap.delayedCall(1, () => { // חכה שנייה (1 שניה)
+              section.setAttribute("anim_btn_completed", "true");
             });
           }
         });
 
-        gsap.from(q("div"), {
-          duration: 0.6,
-          ease: gsap_setting.easing_type,
-          delay: 0.7,
-          opacity: 0,
-        });
       });
     },
     once: true,
   });
-
-
 }
